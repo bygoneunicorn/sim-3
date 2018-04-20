611f7185c9858320433a4fb66f928e1093aa69f8
bygoneunicorn <nathansoccer.jones@gmail.com>
2018-04-20 09:37:03 -0600

    initial push to github

---
+++ server/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const express = require('express')
+    , session = require('express-session')
+    , passport = require('passport')
+    , Auth0Strategy = require('passport-auth0')
+    , massive = require('massive');
+
+const app = express();
+
+
